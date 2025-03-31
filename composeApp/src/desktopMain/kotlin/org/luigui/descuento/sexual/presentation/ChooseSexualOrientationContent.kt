b793d56de761af2647148f1fbb65cee2b315d290
Guillermo Luigui Nieto <guillermo.luigui.nieto@gmail.com>
2025-03-31 16:27:55 -0500

    add buttons

--- composeApp/src/desktopMain/kotlin/org/luigui/descuento/sexual/presentation/ChooseSexualOrientationContent.kt
+++ composeApp/src/desktopMain/kotlin/org/luigui/descuento/sexual/presentation/ChooseSexualOrientationContent.kt
@@ -9,11 +9,20 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.luigui.descuento.sexual.viewmodels.SexualDiscountViewModel
 
 @Composable
@@ -21,6 +30,9 @@ fun ChooseSexualOrientationContent(
     viewModel: SexualDiscountViewModel,
     onNavigateToNextScreen: () -> Unit
 ) {
+    var selectedIndex by remember { mutableIntStateOf(-1) }
+    val options = listOf("Tengo sexo con mujeres.", "Tengo sexo con hombres.")
+
     MaterialTheme{
         Box(
             modifier = Modifier
@@ -36,6 +48,33 @@ fun ChooseSexualOrientationContent(
                     text = "Seleccione su orientación sexual",
                     style = MaterialTheme.typography.headlineLarge,
                 )
+
+                Spacer(
+                    modifier = Modifier.weight(1f)
+                )
+
+                SingleChoiceSegmentedButtonRow(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    options.forEachIndexed { index, label ->
+                        SegmentedButton(
+                            shape = SegmentedButtonDefaults.itemShape(
+                                index = index,
+                                count = options.size
+                            ),
+                            onClick = { selectedIndex = index },
+                            selected = index == selectedIndex,
+                            label = {
+                                Text(
+                                    label,
+                                    style = MaterialTheme.typography.bodyLarge.copy(fontSize = 18.sp),
+                                    maxLines = 1,
+                                    textAlign = TextAlign.Center
+                                )
+                            }
+                        )
+                    }
+                }
 
                 Spacer(
                     modifier = Modifier.weight(1f)
