e7bc9a2df4aac23953eae4a6b8949555d8e1b101
Guillermo Luigui Nieto <guillermo.luigui.nieto@gmail.com>
2025-04-07 12:24:33 -0500

    add button to select file path

--- composeApp/src/desktopMain/kotlin/org/luigui/descuento/sexual/screens/HomeScreen.kt
+++ composeApp/src/desktopMain/kotlin/org/luigui/descuento/sexual/screens/HomeScreen.kt
@@ -5,19 +5,42 @@ import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import org.luigui.descuento.sexual.presentation.HomeScreenContent
 import org.luigui.descuento.sexual.viewmodels.SexualDiscountViewModel
+import java.io.File
+import javax.swing.JFileChooser
+import javax.swing.filechooser.FileSystemView
 
 class HomeScreen(
-    private val viewModel: SexualDiscountViewModel
+    private val viewModel: SexualDiscountViewModel,
 ): Screen {
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.current
 
+        val onSelectFolder = {
+            // Create a JFileChooser instance
+            val fileChooser = JFileChooser(FileSystemView.getFileSystemView())
+            fileChooser.dialogTitle = "Select Folder"
+            fileChooser.fileSelectionMode = JFileChooser.DIRECTORIES_ONLY // Allow only folders
+
+            // Set the default directory to the user's Documents folder
+            val documentsFolder = File(System.getProperty("user.home"), "Documents")
+            fileChooser.currentDirectory = documentsFolder
+
+            // Show the folder picker dialog
+            val result = fileChooser.showOpenDialog(null)
+            if (result == JFileChooser.APPROVE_OPTION) {
+                // Update the ViewModel with the selected folder path
+                val selectedFolder = fileChooser.selectedFile
+                viewModel.updateSelectedFolderPath(selectedFolder.absolutePath)
+            }
+        }
+
         HomeScreenContent(
             viewModel = viewModel,
             onNavigateToNextScreen = {
                 navigator?.push(RegisterParticipantScreen(viewModel))
-            }
+            },
+            onSelectFolder = onSelectFolder
         )
     }
 }
--- composeApp/src/desktopMain/kotlin/org/luigui/descuento/sexual/viewmodels/SexualDiscountViewModel.kt
+++ composeApp/src/desktopMain/kotlin/org/luigui/descuento/sexual/viewmodels/SexualDiscountViewModel.kt
@@ -1,13 +1,55 @@
 package org.luigui.descuento.sexual.viewmodels
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import org.luigui.descuento.sexual.data.SexualOrientation
+import java.io.File
 
 class SexualDiscountViewModel: ViewModel() {
     private val _sexualOrientation = MutableStateFlow<SexualOrientation?>(null)
     val sexualOrientation: StateFlow<SexualOrientation?> = _sexualOrientation
+
+    private var _selectedFolderPath by mutableStateOf<String?>(getDefaultFolderPath())
+    val selectedFolderPath: String? get() = _selectedFolderPath
+
+    private val userHome: String = System.getProperty("user.home")
+    private val storageFile = File(userHome, "user_selected_folder.txt")
+
+    init {
+        // Load the last selected folder path when the ViewModel is created
+        loadSelectedFolderPath()
+    }
+
+    fun updateSelectedFolderPath(path: String) {
+        _selectedFolderPath = path
+        saveSelectedFolderPath(path)
+    }
+
+    private fun saveSelectedFolderPath(path: String) {
+        try {
+            storageFile.writeText(path)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun getDefaultFolderPath(): String {
+        return File(System.getProperty("user.home"), "Documents").absolutePath
+    }
+
+    private fun loadSelectedFolderPath() {
+        try {
+            if (storageFile.exists()) {
+                _selectedFolderPath = storageFile.readText()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
 
     private fun resourceExists(path: String): Boolean {
         return try {
